e6b28f830772773a520965f47015c915d6eb76e9
Javier González Ovalle <javiergov+developer@gmail.com>
2019-12-13 18:05:13 -0300

    Initial Commit

---
+++ NBA Player Fun/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NBA Player Fun
+//
+//  Created by Javier González Ovalle on 13-12-19.
+//  Copyright © 2019 GOv Works. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
